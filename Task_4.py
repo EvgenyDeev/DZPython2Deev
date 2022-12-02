54cfebac33a592d18f53928b0076c957d196a935
Evgeny <deevzhenja@inbox.ru>
2022-12-02 11:04:09 +0300

    Сделал задание 4

---
+++ Task_4.py
@@ -0,0 +1,26 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных индексах. Индексы вводятся одной строкой, через пробел.
+# n = 3
+# [-3, -2, -1, 0, 1, 2, 3]
+# --> 0 2 3
+# -3 * -1 * 0 = 0
+# Вывод: 0
+from random import randint
+numbers = []
+for i in range(11):
+    numbers.append(randint (-10,11))
+print(numbers)
+
+def get_numbers(numbers):
+    count =0 
+    for element in numbers:
+        count +=1
+    return count
+print('Всего элементов: ', get_numbers(numbers))
+
+x = int(input('Введите первый элемент: '))
+y = int(input('Введите второй элемент: '))
+
+for i in range(len(numbers)):
+    mult = numbers[x -1]*numbers[y - 1]
+print(f'Произведение элементов: {numbers[x -1]} * {numbers[y -1]} =', mult)
